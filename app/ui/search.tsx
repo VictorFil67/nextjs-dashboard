1e0f13b988bdf7c0070cf571e0a0980b3440bc21
VictorFil67 <victorfilinovych@gmail.com>
2024-08-21 17:14:30 +0300

    added useSearchParams

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+// import { useRouter } from 'next/router';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+
+      console.log(params.toString());
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
